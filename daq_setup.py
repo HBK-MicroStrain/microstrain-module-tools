01eb6e978e8cf1fe6da2abb2bde9cc217e6d124f
Alex Farrell <ralexfarrell@outlook.com>
2026-04-10 17:02:38 -0400

    Added reload function for hot-reload of modules

--- daq_setup.py
+++ daq_setup.py
@@ -2,6 +2,21 @@ import pathlib
 
 import opendaq as daq
 
-instance_builder = daq.InstanceBuilder()
-instance_builder.module_path = str(pathlib.Path(__file__).parent / "modules")
-instance = instance_builder.build()
+
+MODULE_PATH = str(pathlib.Path(__file__).parent / "modules")
+
+def _create_instance():
+    builder = daq.InstanceBuilder()
+    builder.add_module_path(MODULE_PATH)
+    return builder.build()
+
+def reload():
+    """Reloads the openDAQ instance, picking up any new modules in the modules/ directory.
+
+    Call this after copying a new .dll into modules/ without restarting the session.
+    """
+    global instance
+    instance = _create_instance()
+    print("Session reloaded.")
+
+instance = _create_instance()
