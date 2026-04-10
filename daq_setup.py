917d395db5d70bc22a3b81ca5979751b746557f8
Alex Farrell <ralexfarrell@outlook.com>
2026-04-10 16:30:14 -0400

    Fixed setup issue with module path

--- daq_setup.py
+++ daq_setup.py
@@ -2,5 +2,6 @@ import pathlib
 
 import opendaq as daq
 
-MODULE_PATH = pathlib.Path(__file__).parent / "modules"
-instance = daq.Instance(MODULE_PATH)
+instance_builder = daq.InstanceBuilder()
+instance_builder.module_path = str(pathlib.Path(__file__).parent / "modules")
+instance = instance_builder.build()
